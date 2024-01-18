5a6607a8f836c7193a5c8946aa727ca6e0268862
rqure <64617806+rqure@users.noreply.github.com>
2024-01-17 17:33:16 -0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"context"
+	qmq "qmq/src"
+)
+
+func main() {
+  ctx := context.Background()
+	app := qmq.NewQMQApplication(ctx, "example")
+	app.Initialize(ctx)
+	defer app.Deinitialize(ctx)
+}

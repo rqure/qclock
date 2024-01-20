d2cc4eb3e0140548c3f636799fcb05ff4272075b
rqure <64617806+rqure@users.noreply.github.com>
2024-01-20 00:13:36 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"os"
 	"context"
+	"time"
+	"strconv"
 	qmq "github.com/rqure/qmq/src"
 )
 
 func main() {
 	ctx := context.Background()
+	
 	app := qmq.NewQMQApplication(ctx, "example")
 	app.Initialize(ctx)
 	defer app.Deinitialize(ctx)
+
+	app.AddProducer(ctx, "clock:exchange", 10)
+
+	tickRateMs, err := strconv.Atoi(os.Getenv("TICK_RATE_MS"))
+	if err != nil {
+		tickRateMs = 100
+	}
+	
+	ticker := time.NewTicker(tickRateMs * time.Milliseconds)
+	for {
+		select {
+		case <-ctx.Done:
+			break
+		case t <-ticker.C:
+			timestamp := qmq.QMQTimestamp{Value: t}
+			app.Producer(ctx, "clock:exchange").Push(ctx, &timestamp)
+		}
+	}
 }

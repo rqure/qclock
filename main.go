e0166f2113731d86faec43eabd30f807344263b2
rqure <64617806+rqure@users.noreply.github.com>
2024-01-21 20:15:48 +0000

    progress

--- main.go
+++ main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
+	"context"
 	"os"
 	"os/signal"
-	"context"
-	"time"
 	"strconv"
-	
+	"time"
+
 	qmq "github.com/rqure/qmq/src"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func main() {
 	ctx := context.Background()
-	
+
 	app := qmq.NewQMQApplication(ctx, "clock")
 	app.Initialize(ctx)
 	defer app.Deinitialize(ctx)
 
-	app.AddProducer(ctx, "clock:exchange", 10)
+	app.AddProducer("clock:exchange").Initialize(ctx, 10)
 
 	tickRateMs, err := strconv.Atoi(os.Getenv("TICK_RATE_MS"))
 	if err != nil {
 		tickRateMs = 100
 	}
-	
+
 	sigint := make(chan os.Signal, 1)
-	signal.Notify(sigint, os.Interrupt)	
-	
+	signal.Notify(sigint, os.Interrupt)
+
 	ticker := time.NewTicker(time.Duration(tickRateMs) * time.Millisecond)
 	for {
 		select {
 		case <-ctx.Done():
-			break
+			return
 		case <-sigint:
-			break
+			return
 		case <-ticker.C:
 			timestamp := qmq.QMQTimestamp{Value: timestamppb.Now()}
 			app.Producer("clock:exchange").Push(ctx, &timestamp)

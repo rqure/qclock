730d99ca70c2b1bce680987c10699fd27c5471b1
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-06-27 15:29:59 -0600

    some progress

--- main.go
+++ main.go
@@ -1,87 +1,50 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
-	qmq "github.com/rqure/qmq/src"
-	"google.golang.org/protobuf/types/known/anypb"
-	"google.golang.org/protobuf/types/known/timestamppb"
+	qdb "github.com/rqure/qdb/src"
 )
 
-type NameProvider struct{}
-
-func (np *NameProvider) Get() string {
-	return "clock"
-}
-
-type TimestampToAnyTransformer struct {
-	logger qmq.Logger
-}
-
-func NewTimestampToAnyTransformer(logger qmq.Logger) qmq.Transformer {
-	return &TimestampToAnyTransformer{
-		logger: logger,
-	}
-}
-
-func (t *TimestampToAnyTransformer) Transform(i interface{}) interface{} {
-	d, ok := i.(*qmq.Timestamp)
-	if !ok {
-		t.logger.Error(fmt.Sprintf("TimestampToAnyTransformer: invalid input %T", i))
-		return nil
+func getDatabaseAddress() string {
+	addr := os.Getenv("QMQ_ADDR")
+	if addr == "" {
+		addr = "redis:6379"
 	}
 
-	a, err := anypb.New(d)
-	if err != nil {
-		t.logger.Error(fmt.Sprintf("TimestampToAnyTransformer: failed to marshal timestamp into anypb: %v", err))
-		return nil
-	}
-
-	return a
-}
-
-type TransformerProviderFactory struct{}
-
-func (t *TransformerProviderFactory) Create(components qmq.EngineComponentProvider) qmq.TransformerProvider {
-	transformerProvider := qmq.NewDefaultTransformerProvider()
-	transformerProvider.Set("producer:clock:event:new-timestamp", []qmq.Transformer{
-		NewTimestampToAnyTransformer(components.WithLogger()),
-		qmq.NewAnyToMessageTransformer(components.WithLogger(), qmq.AnyToMessageTransformerConfig{
-			SourceProvider: &NameProvider{},
-		}),
-		qmq.NewTracePushTransformer(components.WithLogger()),
-	})
-	return transformerProvider
-}
-
-type ClockEngineProcessor struct{}
-
-func (c *ClockEngineProcessor) Process(e qmq.EngineComponentProvider) {
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-
-	ticker := time.NewTicker(500 * time.Millisecond)
-
-	for {
-		select {
-		case <-quit:
-			return
-		case <-ticker.C:
-			timestamp := &qmq.Timestamp{Value: timestamppb.Now()}
-			e.WithProducer("clock:event:new-timestamp").Push(timestamp)
-		}
-	}
+	return addr
 }
 
 func main() {
-	engine := qmq.NewDefaultEngine(qmq.DefaultEngineConfig{
-		NameProvider:               &NameProvider{},
-		TransformerProviderFactory: &TransformerProviderFactory{},
-		EngineProcessor:            &ClockEngineProcessor{},
+	db := qdb.NewRedisDatabase(qdb.RedisDatabaseConfig{
+		Address: getDatabaseAddress(),
 	})
-	engine.Run()
+
+	dbWorker := qdb.NewDatabaseWorker(db)
+	leaderElectionWorker := qdb.NewLeaderElectionWorker(db)
+	clockWorker := NewClockWorker(db, 1*time.Second)
+
+	dbWorker.Signals.Connected.Connect(qdb.Slot(leaderElectionWorker.OnDatabaseConnected))
+	dbWorker.Signals.Disconnected.Connect(qdb.Slot(leaderElectionWorker.OnDatabaseDisconnected))
+
+	leaderElectionWorker.Signals.BecameLeader.Connect(qdb.Slot(clockWorker.OnBecameLeader))
+	leaderElectionWorker.Signals.BecameFollower.Connect(qdb.Slot(clockWorker.OnLostLeadership))
+	leaderElectionWorker.Signals.BecameUnavailable.Connect(qdb.Slot(clockWorker.OnLostLeadership))
+
+	// Create a new application configuration
+	config := qdb.ApplicationConfig{
+		Name: "clock",
+		Workers: []qdb.IWorker{
+			dbWorker,
+			leaderElectionWorker,
+			clockWorker,
+		},
+	}
+
+	// Create a new application
+	app := qdb.NewApplication(config)
+
+	// Execute the application
+	app.Execute()
 }
